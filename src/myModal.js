7cb35a7c2cc1027caccd2e1c0342b7b0c2d0bc34
kh45 <khaledhassan45@gmail.com>
2020-09-03 14:57:55 -0400

    made carousel progmatic and added content to modal body

--- src/myModal.js
+++ src/myModal.js
@@ -4,11 +4,21 @@ import Carousel from 'react-bootstrap/Carousel'
 
 class MyModal extends React.Component {
 
+    createSlides = () => {
+        return this.props.project.pictures.map(picture => <Carousel.Item>
+            <img className="d-block w-100" src={require(`${this.props.project.img1}`)} alt="First slide"/>
+        <Carousel.Caption>
+        <h3>First slide label</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+        </Carousel.Item>)
+    }
+
     render() {
         return(
             <Modal show={this.props.showModal} size="lg" onHide={this.props.handleHideParent} dialogClassName="hey">
                 <Modal.Header className="screenshot">
-                    <Carousel>
+                    <Carousel autoPlay={true} interval={2500}>
                         <Carousel.Item>
                             <img className="d-block w-100" src={require(`${this.props.project.img1}`)} alt="First slide"/>
                         <Carousel.Caption>
@@ -33,9 +43,11 @@ class MyModal extends React.Component {
   </Carousel.Item>
 </Carousel>
                 </Modal.Header>
-                <Modal.Body className="yello">
-                    <div className="nigga">
-                    Title of Thing
+                <Modal.Body className="custom-modal-body">
+                    <div>
+                        <h3>{this.props.project.title}</h3>
+                        <h5>React | Redux | Rails | Bootstrap</h5>
+                        <p>What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?</p>
                     </div>
                 </Modal.Body>
                 <Modal.Footer>Close</Modal.Footer>
